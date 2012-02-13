85fa7a574641e648ae3d98383a2594aa4e5c309f
Javier Villanueva <javiervd@gmail.com>
2012-02-13 17:36:09 -0430

    Added sidebar on services page

--- archive-service.php
+++ archive-service.php
@@ -4,6 +4,7 @@
 		<?php roots_main_before(); ?>
 			<div id="main" class="<?php echo $roots_options['main_class']; ?>" role="main">
 				<div class="container">
+                    <hr>
 					<h1>
 						<?php post_type_archive_title(); ?>
 					</h1>
@@ -20,27 +21,41 @@
                         if ( have_posts() ) :
                         
                     ?>
-                    <div id="<?php echo $args['servicetype']; ?>" class="service-type">
-                        <header>
-                            <h2>
-                                Accounting Services for <?php single_term_title(); ?>
-                            </h2>
-                            <?php if($desc != '') : echo $desc; endif; ?>
-                        </header>
-                        <?php while ( have_posts() ) : the_post(); ?>
-                        <div class="service">
-                            <h3>
-                                <?php the_title(); ?>
-                            </h3>
-                            <div class="entry">
-                                <?php the_content(); ?>
+                    <div class="service-container">
+
+                        <div id="<?php echo $args['servicetype']; ?>" class="service-type">
+                            <header>
+                                <h2>
+                                    Accounting Services for <?php single_term_title(); ?>
+                                </h2>
+                                <?php if($desc != '') : echo $desc; endif; ?>
+                            </header>
+                            <?php while ( have_posts() ) : the_post(); ?>
+                            <div class="service">
+                                <h3>
+                                    <?php the_title(); ?>
+                                </h3>
+                                <div class="entry">
+                                    <?php the_content(); ?>
+                                </div>
+                                <!-- <p>
+                                                                <?php echo get_the_term_list( get_the_ID(), 'servicetype', "servicetype: " ) ?>
+                                                            </p> -->
                             </div>
-                            <!-- <p>
-                                                            <?php echo get_the_term_list( get_the_ID(), 'servicetype', "servicetype: " ) ?>
-                                                        </p> -->
+                            <?php endwhile; ?>
                         </div>
-                        <?php endwhile; ?>
-                    </div>
+
+                        <?php roots_sidebar_before(); ?>
+                            <aside class="sidebar <?php echo $roots_options['sidebar_class']; ?>" role="complementary">
+                            <?php roots_sidebar_inside_before(); ?>
+                                <div class="container">
+                                    <?php dynamic_sidebar("Sidebar"); ?>
+                                </div>
+                            <?php roots_sidebar_inside_after(); ?>
+                            </aside><!-- /.sidebar -->
+                        <?php roots_sidebar_after(); ?>
+
+                    </div><!-- /.service-container -->
 
                     <?php 
                         
@@ -53,6 +68,8 @@
                         if ( have_posts() ) :
                     
                     ?>
+                    <hr>
+
                     <div id="<?php echo $args['servicetype']; ?>" class="service-type">
                         <header>
                             <h2>
@@ -71,6 +88,17 @@
                         </div>
                         <?php endwhile; ?>
                     </div>
+
+                    <?php roots_sidebar_before(); ?>
+                        <aside class="sidebar bottom <?php echo $roots_options['sidebar_class']; ?>" role="complementary">
+                        <?php roots_sidebar_inside_before(); ?>
+                            <div class="container">
+                                <?php dynamic_sidebar("Sidebar"); ?>
+                            </div>
+                        <?php roots_sidebar_inside_after(); ?>
+                        </aside><!-- /.sidebar -->
+                    <?php roots_sidebar_after(); ?>
+
 					<?php
 
                         endif;
@@ -81,15 +109,6 @@
 				</div>
 			</div><!-- /#main -->
 		<?php roots_main_after(); ?>
-		<?php roots_sidebar_before(); ?>
-			<aside id="sidebar" class="<?php echo $roots_options['sidebar_class']; ?>" role="complementary">
-			<?php roots_sidebar_inside_before(); ?>
-				<div class="container">
-					<?php get_sidebar(); ?>
-				</div>
-			<?php roots_sidebar_inside_after(); ?>
-			</aside><!-- /#sidebar -->
-		<?php roots_sidebar_after(); ?>
 		</div><!-- /#content -->
 	<?php roots_content_after(); ?>
 <?php get_footer(); ?>
